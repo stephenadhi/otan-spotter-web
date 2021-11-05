0a204aebf70e2fe134ee244aadf8948dc6de48a0
Stephen Adhisaputra <stephen.adhisaputra@stud.uni-due.de>
2021-11-05 15:15:30 +0100

    add makefile

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 mkdir -p ~/.streamlit/
 echo "\
+[general]\n\
+email = \"{stephenadhi@gmail,com}\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
